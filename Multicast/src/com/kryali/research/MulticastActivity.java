30b3073fab7a7e68dbaa501cae26e8d4ed5ef4ce
Kiran Ryali <kiran.ryali@gmail.com>
2011-12-13 00:48:59 -0600

    first android commit

---
+++ Multicast/src/com/kryali/research/MulticastActivity.java
@@ -0,0 +1,13 @@
+package com.kryali.research;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MulticastActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
