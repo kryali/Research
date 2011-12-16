04dc4582cea3959149eb2f0b37a6eb127642a85e
Kiran Ryali <kiran.ryali@gmail.com>
2011-12-16 19:13:35 +0000

    Added node views and updated lots of server logic

--- Tracker/app.js
+++ Tracker/app.js
@@ -26,7 +26,50 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
-hosts = {};
+hosts = [];
+
+function add_host( hosts, host ) {
+
+  for( var i = 0 ; i < hosts.length; i++) {
+    var current_host = hosts[i];
+    if( host.id == current_host.id) {
+      console.log("Host already exists");
+      return;
+    }
+  }
+
+  hosts.push(host);
+}
+
+function find_host( hosts, host_id ) {
+  for( var i = 0 ; i < hosts.length; i++) {
+    if(hosts[i]['id'] == host_id) {
+      return hosts[i];
+    }
+  }
+  return undefined;
+}
+
+function find_node( host, node_id ) {
+  for( var i = 0 ; i < host.nodes.length; i++) {
+    if(host.nodes[i]['id'] == node_id) {
+      return host.nodes[i];
+    }
+  }
+  return undefined;
+}
+
+function add_node( host, node ) {
+  
+  var nodes = host.nodes;
+  for( var i = 0 ; i < nodes.length; i++) {
+    if(nodes[i]['id'] == node.id) {
+      /* Node already exists */
+      return;
+    }
+  }
+  host.nodes.push(node);
+}
 
 // Routes
 app.get('/', function(req, res){
@@ -42,7 +85,12 @@ app.get('/clients', function(req, res) {
 app.post('/hosts/new', function(req, res) {
   console.log( "Received host post" );
   console.log( req.body );
-  hosts[req.body.id] = req.body;
+  var host_id = req.body.id;
+  var host = req.body;//{ host_id: req.body };
+  host.nodes = [];
+  add_host( hosts, host );
+  //req.body['start time'] = new Date(req.body['start time']);
+  //hosts[req.body.id] = req.body;
   res.send("Got request");
 });
 
@@ -56,15 +104,15 @@ app.get('/hosts/', function(req, res) {
 app.get('/hosts.:format?', function( req, res ) {
   if( req.params.format == 'json' ) {
     console.log( "Json request received");
-    res.send( hosts , {'Content-Type':'application/json'}, 200);
+    res.send( { hosts: hosts } , {'Content-Type':'application/json'}, 200);
   };
 });
 
 // This should become post soon
 app.get('/hosts/:id', function(req, res) {
   var id = req.params.id;
-  if( hosts[id] != undefined ) {
-    var host = hosts[id];
+  var host = find_host( hosts, id );
+  if( host != undefined ) {
     res.render('hosts/show', {
       title: 'Showing host id ' + req.params.id,
       host: host
@@ -77,19 +125,36 @@ app.get('/hosts/:id', function(req, res) {
   }
 });
 
-app.get('/hosts/:id/client/new', function(req, res) {
+// This should become post soon
+app.get('/hosts/:host_id/nodes/:node_id', function(req, res) {
+  var host_id = req.params.host_id;
+  var node_id = req.params.node_id;
+  var host = find_host( hosts, host_id );
+  if( host != undefined ) {
+    var node = find_node( host, node_id );
+    if( node != undefined ) {
+      res.render('hosts/show', {
+        title: 'Showing node ' + node_id,
+        host: node
+      });
+      return;
+    }
+  } 
+
+  res.render('error', {
+    title: "Error",
+    error: "Couldn't find node id" +  node_id
+  });
+});
+
+app.post('/hosts/:id/client/new', function(req, res) {
+  console.log( "Got new client request" );
   var id = req.params.id;
-  // TODO: check to see id is within bounds
-  var host = hosts[id];
-  var client = { 
-    id: 0,
-    type: 'CLIENT',
-    start_time: 0,
-    bandwidth: 100,
-    ip: '192.168.1.1',
-    port: 8000,
-  };
-  host.sources.push(client);
+  var host = find_host( hosts, id );
+  if( host != undefined ) {
+    var node = req.body;
+    add_node( host, node );
+  }
 });
 
 // Only listen on $ node app.js
